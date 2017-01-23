581cb0f48e982d49575b78b9092166ea10054742
root <root@comp.domen>
2017-01-23 22:38:30 +0300

    appointments

---
+++ modules/appointments.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules;
+
+/**
+ * 0001 module definition class
+ */
+class appointments extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}

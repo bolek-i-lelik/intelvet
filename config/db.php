4ed5fb87f4139585afcca9dc412589483b0876d8
seblex <burovav84@mail.ru>
2017-01-23 23:06:21 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=intelvet',
-    'username' => 'root',
-    'password' => '050184',
-    'charset' => 'utf8',
-];
